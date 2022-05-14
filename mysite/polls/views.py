d028da482ea113c36c569a831ab857681e4b15af
MuhammedMusharaf007 <mohammedmusharafnilambur@gmail.com>
2022-05-14 10:29:26 +0530

    Filtering first five query view without future polls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -63,6 +63,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from .models import Choice, Question
 
 class IndexView(generic.ListView):
@@ -70,7 +71,8 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte = timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
