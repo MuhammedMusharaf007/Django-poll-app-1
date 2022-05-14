1ff402bacd461420500ee6c056befe0f499437af
MuhammedMusharaf007 <mohammedmusharafnilambur@gmail.com>
2022-05-14 10:08:22 +0530

    First test for checking future question

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from django.utils import timezone
+import datetime
+from .models import Question
 # Create your tests here.
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently returns False for dates in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days = 30)
+        future_question = Question(pub_date = time)
+        self.assertIs(future_question.was_published_recently(), False)
